6645c7804227dcdcd3829a49ccf6d2e5f714bb02
algry <irongeardragon135@gmail.com>
2018-04-07 11:22:27 -0600

    add 'new' command

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const config = require("./config.json");
+const schedule = require("node-schedule");
 const Discord = require('discord.js');
 const client = new Discord.Client();
 client.login(config.token);
@@ -8,10 +9,28 @@ var session = {
 };
 
 client.login(config.token);
+var tasks = [];
 
 client.on('ready', () => {
-  console.log('I am ready!');
-  client.channels.findAll("name", "general").forEach(channel=>{
-	  channel.send("Start Successful");
-  });
+	console.log('I am ready!');
 });
+
+client.on('message', (message) => {
+	if (message.channel.type !== 'dm') return;
+	console.log(message.content);
+	if (!message.content.startsWith('.r/')) return;
+	var args = message.content.split('/');
+	switch(args[1]) {
+		case 'new':
+			console.log(args);
+		
+			var description = args[2];
+			var time = args[3];
+			tasks.push(schedule.scheduleJob(time, ()=>{
+				message.channel.send(description);
+			}));
+			break;
+	}
+});
+
+//.r/new/Description/Time or /Date
