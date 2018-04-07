7bfa0715f0fa30c31b3c698d68932a03b2efadd8
algry <irongeardragon135@gmail.com>
2018-04-07 11:50:31 -0600

    add 'list' and 'del' functions

--- index.js
+++ index.js
@@ -22,15 +22,28 @@ client.on('message', (message) => {
 	var args = message.content.split('/');
 	switch(args[1]) {
 		case 'new':
-			console.log(args);
-		
 			var description = args[2];
 			var time = args[3];
-			tasks.push(schedule.scheduleJob(time, ()=>{
+			var latestTask = tasks.push({time:time,description:description,job:schedule.scheduleJob(time, ()=>{
 				message.channel.send(description);
-			}));
+			})});
+			message.channel.send('Your new reminder number is: ' + latestTask);
+			break;
+		case 'del':
+			tasks[args[2]-1].job.cancel();
+			tasks.splice(args[2]-1, 1);
+			message.channel.send('Deleted task ' + args[2]);
+			break;
+		case 'list':
+			var list = "Tasks: \n";
+			tasks.forEach((task,index)=>{
+				list+="Your task number " + (index+1) + " is '" + task.description + "', and it will be set off at: " + task.time + "\n";
+			})
+			message.channel.send(list);
 			break;
 	}
 });
 
 //.r/new/Description/Time or /Date
+//.r/del/taskNum
+//.r/list
